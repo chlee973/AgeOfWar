c4af3ceece113ba0cf212fc8e336df077ed519d3
chlee973 <chlee973@kaist.ac.kr>
2022-07-17 02:56:02 +0900

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    public void OnClickNinjaSpawn()
+    {
+       
+            Instantiate(prefab, new Vector3(-6, -3, 0), Quaternion.identity);
+    }
+}
